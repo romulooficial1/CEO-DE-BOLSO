68779cbc0448ea38273b712985335f32cef6f953
romulooficial1 <fhh99hhrbm@privaterelay.appleid.com>
2026-06-17 18:30:19 -0300

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:0000000000000000000000"
-};
